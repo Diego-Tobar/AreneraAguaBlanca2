22e61231f73e1b900ceb6c7ace839eeff563c3b0
Diego-Tobar <54144333+Diego-Tobar@users.noreply.github.com>
2019-08-22 13:02:56 -0500

    cambio pagina contacto

--- AreneraAguaBlanca2/Controllers/HomeController.cs
+++ AreneraAguaBlanca2/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace AreneraAguaBlanca2.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "El contacto de nuestra pagina.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "El contacto de nuestra pagina.";
 
             return View();
         }
